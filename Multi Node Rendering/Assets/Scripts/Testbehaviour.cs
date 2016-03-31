3411653099b2741e4aba058427d71fc120590fa5
Bernhard <e0828592@student.tuwien.ac.at>
2016-03-31 14:00:40 +0200

    hopefully last ini

---
+++ Multi Node Rendering/Assets/Scripts/Testbehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testbehaviour : MonoBehaviour
+{
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            this.gameObject.transform.Rotate(Vector3.up, 90.0f * Time.deltaTime);
+        
+    }
+}
